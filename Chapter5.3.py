c6114e5a5cf6182cb53ad097d95bd38ac1061645
oleg_andriushchenko <olegandriushchenko95@gmail.com>
2022-09-21 09:41:36 -0500

    Chapter 5 lesson 3 assignment done

---
+++ Chapter5.3.py
@@ -0,0 +1,37 @@
+
+list = []
+def print_sum_avg(list):
+
+result = 0
+for x in list:
+    result += x
+return result
+
+result = 0
+for x in list:
+    result /= x
+return result
+
+list = user_input
+print(print_sum_avg(list))
+
+user_input = input('Add 3 numbers\n'):
+print("Thank you")
+
+list = int(user_input):
+
+#Exercise 2
+def print_sum_avg_arg(*args):
+
+    result = 0
+    for x in args:
+        result += x
+    return result
+print(print_sum_avg_arg(list))
+
+
+#Exercise 4
+user_input = input("What you studying?\n")
+print("I am studying " + user_input)
+
+user_answer = str(user_input):
