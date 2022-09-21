aec3fa440c0a3605878cab1d354a1e39db14001c
oleg_andriushchenko <olegandriushchenko95@gmail.com>
2022-09-21 13:40:07 -0500

    correcting

--- rectangle.py
+++ rectangle.py
@@ -1,8 +1,8 @@
 class Rectangle:
-    def __init__(self, width, length)
+    def __init__(self, width, length):
         self.width = width
         self.lenth = length
-    def area(self)
+    def area(self):
         print(self.width * self.lenth)
 
 rectangle1 = Rectangle(4, 7)
