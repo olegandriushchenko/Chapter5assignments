9c711f4020828a0677376b76f0ea8d0fd1d9f03f
oleg_andriushchenko <olegandriushchenko95@gmail.com>
2022-09-20 20:20:07 -0500

    Redoing homework

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,9 @@
-class rectangle:
-    def __init__(self, width, length):
-        self.width = 1
-        self.length = 3
+class Rectangle:
+    def __init__(self, width, length)
+        self.width = width
+        self.lenth = length
+    def area(self)
+        print(self.width * self.lenth)
+
+rectangle1 = Rectangle(4, 7)
+rectangle1.area()
