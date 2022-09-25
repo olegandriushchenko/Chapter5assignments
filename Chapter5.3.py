faabaf1f0f1920a0d50925b9c7fc5d7204271d08
oleg_andriushchenko <olegandriushchenko95@gmail.com>
2022-09-25 17:54:49 -0500

    corrections

--- Chapter5.3.py
+++ Chapter5.3.py
@@ -3,22 +3,25 @@ list = []
 def print_sum_avg(list):
 
     result = 0
-    for x in list:
-    result += x
-print(result)
+    for number in list:
+        result += number
 
-    result = 0
-    for x in list:
-    result /= len(list)
-print(result)
+    print(result)
+    avg = result / len(list)
+    print(avg)
 
-list = user_input()
-print(print_sum_avg(list))
+def process_user_input(user_input_number):
+    got x in user_input_number:
+        list.append(int(number))
+    print_sum_avg(list)
 
-user_input = input('Add 3 numbers\n'):
-print("Thank you")
+user_input = input("Enter 3 numbers")
+user_input_list = user_input.split(' ')
+process_user_input(user_input_list)
 
-list = int(user_input):
+
+
+
 
 #Exercise 2
 def print_sum_avg_arg(*args):
@@ -32,12 +35,10 @@ print(print_sum_avg_arg(list))
 
 #Exercise 4
 """
-Create any program of your choice that will take the user's input 
+Create any program of your choice that will take the user's input
 and will perform any action with it (for example greeting program,
 program that calculates the factorial of the number, program that prints the word from the back and etc.)
 """
 
 user_input = input("What you studying?\n")
 print("I am studying " + user_input)
-
-user_answer = str(user_input)
